d1c3bd53fdf4cfd0149168d89f5ee3a3a8837f34
PlutoCtx <chentingxian195467@163.com>
2023-02-03 23:40:28 +0800

    导入第三方框架Buefy、VueUI、bulma和materialdesignicons的css、js样式

--- app/blog/views.py
+++ app/blog/views.py
@@ -4,7 +4,7 @@ bp = Blueprint('blog', __name__, url_prefix='/blog', template_folder='templates'
 
 
 def index():
-    return render_template('index.html')
+    return render_template('../templates/index.html')
 
 # it's a flask project.And for ,it's the first time to use a flask frame to build a project.
 # It's a personal blog project,in some words,it's a simple one,but new to me.
