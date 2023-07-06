df89f548d3b154a324c854a1810657f8390d29c5
PlutoCtx <chentingxian195467@163.com>
2023-07-06 15:26:16 +0800

    Flask-Migrate, 数据库连接有问题，似乎是编码方式的原因

--- RealProject/__init__.py
+++ RealProject/__init__.py
@@ -6,37 +6,42 @@
 # @Software: PyCharm
 # @User: chent
 
-from flask import Flask
 import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from RealProject.settings import BASE_DIR
+from flask_migrate import Migrate
+
+# 安装SQLAlchemy对象
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app(test_config=None):
     # create and configure the app
     # instance_relative_config 设置为True则代表开启文件加载配置，默认为False
     app = Flask(__name__, instance_relative_config=True)
 
-    # app.config其实调用的是Flask类的config属性，该属性又被设置为了一个config的类
-    # from_mapping则是该config类下的一个方法，用来更新默认配置，返回值为True
-    # 至于Flask的默认配置都有哪些，其实可以深入源码查看default_config属性所列出的项
-    # 默认配置
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABA5E=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
     # 这里做了判断是否运行时传入了测试配置
     if test_config is None:
         # 如果没有传入，则从py文件加载配置，silent = True代表文件，文件加载成功则返回True
-        app.config.from_pyfile('settings.py', silent=True)
+        CONFIG_PATH = BASE_DIR / 'RealProject/settings.py'
+        app.config.from_pyfile(CONFIG_PATH, silent=True)
     else:
         # 和最开始的配置意思一致
         app.config.from_mapping(test_config)
 
-    # 递归创建目录，确保文件项目存在
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # 绑定数据库
+    db.init_app(app)
+    migrate.init_app(app, db)
 
+    # 引入blog视图文件
     from app.blog import views as blog
     app.register_blueprint(blog.bp)
+
+    # 注册数据库模型
+    from app.blog import models
 
     return app
--- RealProject/settings.py
+++ RealProject/settings.py
@@ -5,3 +5,12 @@
 # @File: settings.py
 # @Software: PyCharm
 # @User: chent
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = True
+SECRET_KEY = '1%3ya7fn3moipdpcltj(tdfcv5^@lj=t5d&72levvls+y*@4^'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/flaskdb'
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
